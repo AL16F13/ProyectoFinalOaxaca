d9ee5d8ee719b03e9b0ebfd36040419a49b5d32f
AL16F13 <julyev.2508.guerrero@gmail.com>
2015-06-24 09:37:42 -0500

    uno

---
+++ js/acciones.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	
+	
+	$ ('#oaxaca') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+$ ('#guelaguetza') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+$ ('#tule') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+$ ('#soledad') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+$ ('#monte') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+$ ('#domingo') .on ('tap' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+
+}); 
+});
+
